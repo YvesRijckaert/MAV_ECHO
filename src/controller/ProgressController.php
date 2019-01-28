32732c77574d1088d1746853c81c551bf66d5838
Yves Rijckaert <yves.rijckaert@student.howest.be>
2019-01-28 13:18:51 +0100

    better normalize file for css

--- src/controller/ProgressController.php
+++ src/controller/ProgressController.php
@@ -7,31 +7,32 @@ require_once __DIR__ . '/../dao/HabitDAO.php';
 class ProgressController extends Controller {
 
   public function progress() {
-        if (empty($_SESSION['user'])) {
-            $_SESSION['error'] = 'You have to be signed in.';
-            header('Location: index.php');
-            exit();
+    if (empty($_SESSION['user'])) {
+        $_SESSION['error'] = 'You have to be signed in.';
+        header('Location: index.php');
+        exit();
+    } else {
+        if (!empty($_GET['category'])) {
+            switch ($_GET['category']) {
+        case 'statistics':
+          $this->set('currentCategory', 'statistics');
+          break;
+        case 'achievements':
+          $this->set('currentCategory', 'achievements');
+          break;
+        case 'goals':
+        //get goals
+          $this->set('currentCategory', 'goals');
+          break;
+        default:
+          header('Location: index.php?page=progress&category=statistics');
+          break;
+      }
         } else {
-            if (!empty($_GET['category'])) {
-                switch ($_GET['category']) {
-            case 'statistics':
-              $this->set('currentCategory', 'statistics');
-              break;
-            case 'achievements':
-              $this->set('currentCategory', 'achievements');
-              break;
-            case 'goals':
-              $this->set('currentCategory', 'goals');
-              break;
-            default:
-              header('Location: index.php?page=progress&category=statistics');
-              break;
-          }
-            } else {
-                header('Location: index.php?page=progress&category=statistics');
-            }
+            header('Location: index.php?page=progress&category=statistics');
         }
-        $this->set('title', 'Progress');
-        $this->set('currentPage', 'progress');
     }
+    $this->set('title', 'Progress');
+    $this->set('currentPage', 'progress');
+  }
 }
