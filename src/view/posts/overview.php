a294512773d4968789b6352fbd9e8ea4f17e9e21
Yves Rijckaert <yves.rijckaert@student.howest.be>
2019-02-09 14:11:03 +0100

    breaking lines in short memory

--- src/view/posts/overview.php
+++ src/view/posts/overview.php
@@ -52,37 +52,42 @@
           <title>Collage</title>
           <desc>Collage of the day.</desc>
           <?php if(!empty($fulfilledHabitsOfEnteredDay)): ?>
-              <?php foreach($fulfilledHabitsOfEnteredDay as $habit):
-                switch ($habit['habit_colour_name']) {
-                  case 'blue':
-                    echo '<g class="blue"><circle fill="#4285ff" cx="90" cy="269" r="90"></circle></g>';
-                    break;
-                  case 'orange':
-                    echo '<g class="orange"><path fill="#FBC649"  d="M90.72,360 L180,360 L180,540 L90.72,540 C90.4802221,539.999059 90.240221,540 90,540 C40.2943725,540 -1.8189894e-12,499.705627 -1.8189894e-12,450 C-1.8189894e-12,400.294373 40.2943725,360 90,360 C90.240221,360 90.4802221,360.000941 90.72,360.00282 Z" transform="translate(90.000000, 450.000000) rotate(-90.000000) translate(-90.000000, -450.000000) "></path></g>';
-                    break;
-                  case 'purple':
-                    echo '<g class="purple"><rect fill="#9278fd" x="180" y="0" width="180" height="180"></rect></g>';
-                    break;
-                  case 'green':
-                    echo '<g class="green"><rect fill="#00d28b" x="-1.56319402e-13" y="9.9475983e-14" width="180" height="180"></rect><rect fill="#ffffff" x="-1.56319402e-13" y="9.9475983e-14" width="90" height="90"></rect><rect fill="#ffffff" x="90" y="90" width="90" height="90"></rect></g>';
-                    break;
-                  case 'red':
-                    echo '<g class="red"><polygon fill="#fe5455" points="180 179 360 359 180 359"></polygon></g>';
-                    break;
-                  default:
-                    break;
-                } ?>
-              <?php endforeach; ?>
+            <?php foreach($fulfilledHabitsOfEnteredDay as $habit):
+              switch ($habit['habit_colour_name']) {
+                case 'blue':
+                  echo '<g class="blue"><circle fill="#4285ff" cx="90" cy="269" r="90"></circle></g>';
+                  break;
+                case 'orange':
+                  echo '<g class="orange"><path fill="#fbc649"  d="M90.72,360 L180,360 L180,540 L90.72,540 C90.4802221,539.999059 90.240221,540 90,540 C40.2943725,540 -1.8189894e-12,499.705627 -1.8189894e-12,450 C-1.8189894e-12,400.294373 40.2943725,360 90,360 C90.240221,360 90.4802221,360.000941 90.72,360.00282 Z" transform="translate(90.000000, 450.000000) rotate(-90.000000) translate(-90.000000, -450.000000) "></path></g>';
+                  break;
+                case 'purple':
+                  echo '<g class="purple"><rect fill="#9278fd" x="180" y="0" width="180" height="180"></rect></g>';
+                  break;
+                case 'green':
+                  echo '<g class="green"><rect fill="#00d28b" x="-1.56319402e-13" y="9.9475983e-14" width="180" height="180"></rect><rect fill="#ffffff" x="-1.56319402e-13" y="9.9475983e-14" width="90" height="90"></rect><rect fill="#ffffff" x="90" y="90" width="90" height="90"></rect></g>';
+                  break;
+                case 'red':
+                  echo '<g class="red"><polygon fill="#fe5455" points="180 179 360 359 180 359"></polygon></g>';
+                  break;
+                default:
+                  break;
+              } ?>
+            <?php endforeach; ?>
           <?php endif; ?>
-            <g class="textbox">
-              <rect fill="#ffffff" x="90" y="90" width="182" height="367"></rect>
-              <text font-family="Circular Std" font-size="16" font-style="italic" font-weight="300" fill="#2b2b2b">
-                  <tspan x="100" y="172">#<?php echo $livedDaysAmount; ?></tspan>
-              </text>
-              <text font-family="Circular Std" font-size="22" font-weight="400" line-spacing="24" fill="#2b2b2b">
-                  <tspan x="100" y="244"><?php echo $postOfEnteredDay['short_memory']; ?></tspan>
-              </text>
-            </g>
+        <g class="textbox">
+          <rect fill="#ffffff" x="90" y="90" width="182" height="367"></rect>
+          <text font-family="Circular Std" font-size="16" font-style="italic" font-weight="300" fill="#2b2b2b">
+            <tspan x="100" y="172">#<?php echo $livedDaysAmount; ?></tspan>
+          </text>
+          <text font-family="Circular Std" font-size="22" font-weight="400" line-spacing="24" fill="#2b2b2b">
+            <?php $count = 200; $splittedMemory = explode( "\n", wordwrap( $postOfEnteredDay['short_memory'], 15));
+            foreach ($splittedMemory as $key => $splittedMemory):
+              $count+= 22;
+              echo '<tspan x="100" y=' . $count  . '>' . $splittedMemory . '</tspan>';
+            endforeach;
+            ?>
+          </text>
+        </g>
       </svg>
       </section>
       <section class="main-overview-day-info">
