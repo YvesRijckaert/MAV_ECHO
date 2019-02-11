879420eb5540d9fe4b264315f34232c7d0ff5863
Arthur <arthur.segaert@student.howest.be>
2019-02-11 20:57:52 +0100

    css login

--- src/view/users/login.php
+++ src/view/users/login.php
@@ -1,17 +1,17 @@
 <section>
-  <header><h1 class="page-header">Login</h1></header>
+  <header><h1 class="page-header login-title">Login</h1></header>
     <form class="login-form" method="post" action="index.php?page=login">
       <input type="hidden" name="action" value="login" />
       <div class="input-container text">
         <label>
-          <span class="form-label hidden">Email:</span>
-          <input type="email" autocomplete="on" name="email" placeholder="email" class="form-input" required />
+          <span class="form-label hidden">Email</span>
+          <input class="info-form-input" type="email" autocomplete="on" name="email" class="form-input" required />
         </label>
       </div>
       <div class="input-container text">
         <label>
-          <span class="form-label hidden">Password:</span>
-          <input type="password" autocomplete="on" name="password" placeholder="password" class="form-input" required />
+          <span class="form-label hidden">Password</span>
+          <input class="info-form-input" type="password" autocomplete="on" name="password" class="form-input" required />
         </label>
       </div>
       <div class="input-container submit">
